a57ef83869790be5f82b09f234e633596f14e34b
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-19 15:19:58 +0200

    maj database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :reviews, :bookings, :guitars
+  has_many :reviews
+  has_many :bookings
+  has_many :guitars
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
