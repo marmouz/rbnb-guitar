8fd7ca87c120be56bc7930af3e8d66e7f736f5c9
marine monbailly <marinemonbailly@gmail.com>
2019-08-19 17:32:34 +0200

    profil controller, review controller, routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.booking = @booking
+    @review.save
+    # if @review.save
+    #   redirect_to
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
