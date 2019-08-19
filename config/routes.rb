60e5f81dc42d96d8049086d1daab5bba2706433f
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-19 17:33:28 +0200

    controller bookings created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ test/controllers/bookings_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get bookings_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get bookings_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get bookings_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get bookings_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get bookings_destroy_url
+    assert_response :success
+  end
+
+end
