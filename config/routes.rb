55c95ea9c64e8f6b2805a0d3264d3caf53843a34
marine monbailly <marinemonbailly@gmail.com>
2019-08-19 17:40:03 +0200

    correction typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guitar w do
+  resources :guitar do
     resources :booking, only: [:index, :new, :create]
   end
 
