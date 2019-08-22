264f01acff883c7d6303b899efa641a2bc3d21f9
nicolas schmitt <schmitt_953@hotmail.com>
2019-08-22 11:05:29 +0200

    photo, description added to guitar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :guitars
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :description, presence: true
   validates :photo, presence: true
   mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
---
+++ db/migrate/20190822090136_add_description_to_guitars.rb
@@ -0,0 +1,6 @@
+class AddDescriptionToGuitars < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :description, :string
+    add_column :users, :photo, :string
+  end
+end
